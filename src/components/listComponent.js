4e030b9f476335db87c6721d0cc5bc0615eb3a27
Pasan Jayathilaka <pasan4all@gmail.com>
2020-02-26 17:08:05 +0530

    26-02-20 1707hrs

---
+++ src/components/listComponent.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class listComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> listComponent </Text>
+      </View>
+    );
+  }
+}
+
+export default listComponent;
